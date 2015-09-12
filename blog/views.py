83005ccd342b0bb17a72220d8e132febf89c286d
Robert Jerovsek <robert.jerovsek@yogiyo.co.kr>
2015-09-12 16:50:53 +0900

    Fixes

--- blog/views.py
+++ blog/views.py
@@ -24,7 +24,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
@@ -40,7 +39,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,6 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
